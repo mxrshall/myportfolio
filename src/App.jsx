e41969494265c88191e88ebb81339a97b6db48ee
Martin Krajčovič <martin22.krajcovic@gmail.com>
2025-07-14 00:31:51 +0200

    Pridanie navigacie

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Intro from "./Intro";
 import Homepage from "./Homepage";
+import Navigation from "./Navigation";
 
 export default function App() {
 
   return (
     <>
       <Intro />
+      <Navigation />
       <Homepage />
     </>
   );
---
+++ src/Navigation.jsx
@@ -0,0 +1,12 @@
+export default function Navigation() {
+    return (
+        <nav className="w-full h-20 flex items-center justify-center">
+            <ul className="flex space-x-8 text-lg font-semibold">
+                <li>About Me</li>
+                <li>Skills</li>
+                <li>My Projects</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    );
+}
