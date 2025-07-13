215326c721c1356ea7fc0b3dbf7a7447a75f51f8
Martin Krajčovič <martin22.krajcovic@gmail.com>
2025-07-14 00:48:04 +0200

    Pridanie homepagu

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -1,24 +1,11 @@
-import { useState } from "react";
-
 export default function Homepage() {
-  const [hodnota, setHodnota] = useState(5);
-  const [volanie, setVolanie] = useState("Ano");
-
-  const funkcia = () => {
-    setHodnota(hodnota + 1);
-    if (hodnota > 10) {
-      setVolanie("Ano, hodnota je väčšia ako 10");
-    }
-  };
-
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline text-red-500">
-        Hello world!
-      </h1>
-      <p>{hodnota}</p>
-      <button onClick={funkcia}>Klik</button>
-      <p>{volanie}</p>
+    <div className="w-full h-screen flex items-end justify-start relative">
+      <div className="w-1/3 h-2/3 bg-[#E9B872] absolute top-0 left-1/2 -translate-x-1/2" />
+      <div className="w-4/5 h-4/5 bg-[#F4F1DE] flex flex-col items-start justify-end px-10 py-20">
+        <h1 className="text-9xl font-bold z-20">Hi, my name is Martin</h1>
+        <p className="text-2xl mt-5">Front End Developer</p>
+      </div>
     </div>
   );
 }
--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -1,6 +1,6 @@
 export default function Navigation() {
     return (
-        <nav className="w-full h-20 flex items-center justify-center">
+        <nav className="w-full fixed h-20 flex items-center justify-center z-10">
             <ul className="flex space-x-8 text-lg font-semibold">
                 <li>About Me</li>
                 <li>Skills</li>
