8db3fe087953405b23d709a010cb5786b37ac8a4
Martin Krajčovič <martin22.krajcovic@gmail.com>
2025-07-15 00:40:42 +0200

    Pridanie dat do karieť

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,19 +1,47 @@
 import { FaHtml5 } from "react-icons/fa";
 import { FaCss3Alt } from "react-icons/fa";
 import { FaWordpress } from "react-icons/fa";
+import { RiTailwindCssFill } from "react-icons/ri";
+import { FaPhp } from "react-icons/fa";
+import { SiMysql } from "react-icons/si";
+import { FaJs } from "react-icons/fa";
+import { FaReact } from "react-icons/fa";
+import { SiSass } from "react-icons/si";
+import { FaGithub } from "react-icons/fa";
 
 export default function Card({ title, skill }) {
     return (
-        <div className="w-[20%] h-1/2 bg-white rounded-xl flex flex-col">
+        <div className="w-[17%] h-1/2 bg-white rounded-xl flex flex-col">
             <div className="w-full h-full flex justify-center items-center">
                 {title == "HTML" &&
-                    <FaHtml5 size={100} />
+                    <FaHtml5 size={80} />
                 }  
                 {title == "CSS" &&
-                    <FaCss3Alt size={100} />
+                    <FaCss3Alt size={80} />
                 }
-                {title == "WP" &&
-                    <FaWordpress size={100} />
+                {title == "Tailwind" &&
+                    <RiTailwindCssFill size={80} />
+                }
+                {title == "JavaScript" &&
+                    <FaJs size={80} />
+                }
+                {title == "ReactJS" &&
+                    <FaReact size={80} />
+                }
+                {title == "WordPress" &&
+                    <FaWordpress size={80} />
+                }
+                {title == "SASS" &&
+                    <SiSass size={80} />
+                }
+                {title == "PHP" &&
+                    <FaPhp size={80} />
+                }
+                {title == "MySQL" &&
+                    <SiMysql size={80} />
+                }
+                {title == "Git" &&
+                    <FaGithub size={80} />
                 }
             </div>
             <div className="w-full space-x-1 flex justify-center items-center pb-4">
--- src/Skills.jsx
+++ src/Skills.jsx
@@ -3,13 +3,60 @@ import SectionTitle from "./SectionTitle";
 import Card from "./Card";
 
 export default function Skills() {
+  const skills = [
+    {
+      title: "HTML",
+      skill: 3,
+    },
+    {
+      title: "CSS",
+      skill: 3,
+    },
+    {
+      title: "Tailwind",
+      skill: 3,
+    },
+    {
+      title: "JavaScript",
+      skill: 2,
+    },
+    {
+      title: "ReactJS",
+      skill: 2,
+    },
+    {
+      title: "WordPress",
+      skill: 2,
+    },
+    {
+      title: "SASS",
+      skill: 2,
+    },
+    {
+      title: "PHP",
+      skill: 1,
+    },
+    {
+      title: "MySQL",
+      skill: 1,
+    },
+    {
+      title: "Git",
+      skill: 1,
+    }
+  ]
+
   return (
     <div className="w-full h-screen bg-[#E9B872] flex flex-col items-start justify-start relative px-10 pt-20">
       <SectionTitle title="Skills."/>
-        <div className="w-full h-full flex mt-5">
-          <Card title={"HTML"} skill={3} />
-          <Card title={"CSS"} skill={3} />
-          <Card title={"WP"} skill={2} />
+        <div className="w-full h-4/6 flex mt-5 flex-wrap justify-center items-center gap-12">
+          {skills.map((item, index) => (
+            <Card 
+              key={index}
+              title={item.title}
+              skill={item.skill}          
+            />
+          ))}
         </div>
     </div>
   );
