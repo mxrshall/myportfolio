fe4451da7fe8d29bf53f3f78e88b232905eace83
Martin Krajčovič <martin22.krajcovic@gmail.com>
2025-07-17 14:19:55 +0200

    Scroll navigacie + linky

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -15,9 +15,15 @@ export default function Contact() {
           Did you like one of my projects, have a question, or an idea for collaboration? I’d love to hear from you – I’ll get back to you as soon as possible!
         </div>
         <div className="flex justify-center items-center gap-3 mt-10">
-          <IoMdMail size={30} color="white" />
-          <FaGithub size={30} color="white" />
-          <FaLinkedin size={30} color="white" />
+          <a href="mailto:martin22.krajcovic@gmail.com">
+            <IoMdMail size={30} className="text-white hover:text-[#E9B872] transition-colors duration-300" />
+          </a>
+          <a href="https://github.com/mxrshall" target="_blank">
+            <FaGithub size={30} className="text-white hover:text-[#E9B872] transition-colors duration-300" />
+          </a>
+          <a href="https://www.linkedin.com/in/martin-kraj%C4%8Dovi%C4%8D-8493a4374" target="_blank">
+            <FaLinkedin size={30} className="text-white hover:text-[#E9B872] transition-colors duration-300" />
+          </a>
         </div>
       </div>
     </div>
--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -1,38 +1,49 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-scroll";
 
 export default function Navigation() {
-    return (
-        <nav className="w-full fixed h-20 flex items-center justify-center z-40">
-            <ul className="w-1/3 h-16 bg-[#E9B872] rounded-full flex justify-center items-center space-x-8 text-lg font-semibold cursor-pointer">
-                <Link
-                    to="about"
-                    smooth={true}
-                    duration={500}
-                >
-                    About Me
-                </Link>
-                <Link
-                    to="skills"
-                    smooth={true}
-                    duration={500}
-                >
-                    Skills
-                </Link>
-                <Link
-                    to="projects"
-                    smooth={true}
-                    duration={500}
-                >
-                    My Projects
-                </Link>
-                <Link
-                    to="contact"
-                    smooth={true}
-                    duration={500}
-                >
-                    Contact
-                </Link>
-            </ul>
-        </nav>
-    );
+  const [showNav, setShowNav] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  // Scroll logic
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+
+      if (currentScrollY > lastScrollY && currentScrollY > 100) {
+        setShowNav(false);
+      } else {
+        setShowNav(true);
+      }
+
+      setLastScrollY(currentScrollY);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [lastScrollY]);
+
+  return (
+    <nav
+      className={`w-full fixed top-0 transition-transform duration-300 z-40 ${
+        showNav ? "translate-y-0" : "-translate-y-full"
+      }`}
+    >
+      <ul className="w-1/3 h-16 mx-auto mt-4 bg-[#E9B872] rounded-full flex justify-center items-center space-x-8 text-lg font-semibold cursor-pointer">
+        <Link to="about" smooth={true} duration={500}>
+          About Me
+        </Link>
+        <Link to="skills" smooth={true} duration={500}>
+          Skills
+        </Link>
+        <Link to="projects" smooth={true} duration={500}>
+          My Projects
+        </Link>
+        <Link to="contact" smooth={true} duration={500}>
+          Contact
+        </Link>
+      </ul>
+    </nav>
+  );
 }
